bd49070737ef04f16235bbe1f2ff31a0dd9ef3c4
Sana Urooj <sanaurooj049@gmail.com>
2025-07-31 21:11:57 +0500

    Remove frontend submodule

--- frontend/README.md
+++
@@ -1,7 +0,0 @@
-To run the frontend project
-node v20.19.0 used
-
-open the terminal
-cd frontend
-npm i
-npm run dev
--- frontend/src/hooks/redux.ts
+++
@@ -1,5 +0,0 @@
-import { useSelector, useDispatch, TypedUseSelectorHook } from 'react-redux';
-import type { RootState, AppDispatch } from '../store/store';
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useAppDispatch = () => useDispatch<AppDispatch>();
